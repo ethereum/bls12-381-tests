4468479e68f6c65d7eb20118b13da591307d526f
Antonio Sanso <antonio.sanso@gmail.com>
2024-03-04 13:24:36 +0100

    add g2

--- main.py
+++ main.py
@@ -205,21 +205,7 @@ def case01_add_G1():
         "Expected": int_to_hex(int(result_doubling_P1[0]), 64) + (int_to_hex(int(result_doubling_P1[1]), 64)),
         "Gas": BLS12_G1ADD_GAS,
         "NoBenchmark": False
-        },
-        {
-        "Input": int_to_hex(int(G1[0]), 64) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(G1[0]), 64) + (int_to_hex(int(neg(G1)[1]), 64)),
-        "Name": "bls_g1add_(g1+(-g1)=0)",
-        "Expected": int_to_hex(0, 64) + int_to_hex(0, 64),
-        "Gas": BLS12_G1ADD_GAS,
-        "NoBenchmark": False
-        },
-        {
-        "Input": int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(neg(P1)[1]), 64)),
-        "Name": "bls_g1add_(p1+(-p1)=0)",
-        "Expected": int_to_hex(0, 64) + int_to_hex(0, 64),
-        "Gas": BLS12_G1ADD_GAS,
-        "NoBenchmark": False
-        },
+        }
     ]
 
 
@@ -269,6 +255,13 @@ def case02_add_G2():
         "Input": int_to_hex(int(P2[0].coeffs[0]), 64) + int_to_hex(int(P2[0].coeffs[1]), 64) + int_to_hex(int(P2[1].coeffs[0]), 64) + int_to_hex(int(P2[1].coeffs[1]), 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64),
         "Name": "bls_g2add_(p2+0=p2)",
         "Expected": int_to_hex(int(P2[0].coeffs[0]), 64) + int_to_hex(int(P2[0].coeffs[1]), 64) + int_to_hex(int(P2[1].coeffs[0]), 64) + int_to_hex(int(P2[1].coeffs[1]), 64),
+        "Gas": BLS12_G2ADD_GAS,
+        "NoBenchmark": False
+        },
+        {
+        "Input": int_to_hex(int(G2[0].coeffs[0]), 64) + int_to_hex(int(G2[0].coeffs[1]), 64) + int_to_hex(int(G2[1].coeffs[0]), 64) + int_to_hex(int(G2[1].coeffs[1]), 64) + int_to_hex(int(neg(G2)[0].coeffs[0]), 64) + int_to_hex(int(neg(G2)[0].coeffs[1]), 64) + int_to_hex(int(neg(G2)[1].coeffs[0]), 64) + int_to_hex(int(neg(G2)[1].coeffs[1]), 64),
+        "Name": "bls_g2add_(g2-g2=0)",
+        "Expected": int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64),
         "Gas": BLS12_G2ADD_GAS,
         "NoBenchmark": False
         },
