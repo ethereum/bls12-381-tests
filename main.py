ec75953bcfef303b78a0da8102d1a2d477ab1a6f
Antonio Sanso <antonio.sanso@gmail.com>
2024-04-23 14:19:07 +0200

    fail pairing

--- main.py
+++ main.py
@@ -1395,6 +1395,14 @@ def case18_fail_pairing_check():
             int(neg(G2)[0].coeffs[1]), 64) + int_to_hex(int(neg(G2)[1].coeffs[0]), 64) + int_to_hex(int(neg(G2)[1].coeffs[1]), 64),
         "ExpectedError": "g1 point is not on correct subgroup",
         "Name": "bls_pairing_g1_not_in_correct_subgroup"
+        },
+        {
+        "Input": int_to_hex(int(G1[0]), 64) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(G2_wrong_order[0].coeffs[0]), 64) + int_to_hex(
+            int(G2_wrong_order[0].coeffs[1]), 64) + int_to_hex(int(G2_wrong_order[1].coeffs[0]), 64) + int_to_hex(int(G2_wrong_order[1].coeffs[1]), 64) + int_to_hex(
+            int(G1[0]), 64) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(neg(G2)[0].coeffs[0]), 64) + int_to_hex(
+            int(neg(G2)[0].coeffs[1]), 64) + int_to_hex(int(neg(G2)[1].coeffs[0]), 64) + int_to_hex(int(neg(G2)[1].coeffs[1]), 64),
+        "ExpectedError": "g2 point is not on correct subgroup",
+        "Name": "bls_pairing_g2_not_in_correct_subgroup"
         }
     ]
 
