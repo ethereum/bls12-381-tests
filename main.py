1a6d0c356ddfad36b6e83240f427f0a800d84438
Justin Traglia <jtraglia@pm.me>
2025-02-27 15:02:29 -0600

    Add single pair, point at inf tests

--- main.py
+++ main.py
@@ -1027,6 +1027,32 @@ def case09_pairing_check():
 
     yield 'pairing_check_bls', [
         {
+        "Input": ""
+            # G1 point (point at infinity)
+            + int_to_hex(0, 128)
+            # G2 point
+            + int_to_hex(int(G2[0].coeffs[0]), 64)
+            + int_to_hex(int(G2[0].coeffs[1]), 64)
+            + int_to_hex(int(G2[1].coeffs[0]), 64)
+            + int_to_hex(int(G2[1].coeffs[1]), 64),
+        "Name": "bls_pairing_e(0,G2)",
+        "Expected": int_to_hex(1, 32),
+        "Gas": 1 * BLS12_PAIRING_VARIABLE + BLS12_PAIRING_CONSTANT,
+        "NoBenchmark": False
+        },
+        {
+        "Input": ""
+            # G1 point
+            + int_to_hex(int(G1[0]), 64)
+            + int_to_hex(int(G1[1]), 64)
+            # G2 point (point at infinity)
+            + int_to_hex(0, 256),
+        "Name": "bls_pairing_e(G1,0)",
+        "Expected": int_to_hex(1, 32),
+        "Gas": 1 * BLS12_PAIRING_VARIABLE + BLS12_PAIRING_CONSTANT,
+        "NoBenchmark": False
+        },
+        {
         "Input": int_to_hex(int(G1[0]), 64) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(0, 256) + int_to_hex(0, 128) + int_to_hex(int(G2[0].coeffs[0]), 64) + int_to_hex(
             int(G2[0].coeffs[1]), 64) + int_to_hex(int(G2[1].coeffs[0]), 64) + int_to_hex(int(G2[1].coeffs[1]), 64),
         "Name": "bls_pairing_e(G1,0)=e(0,G2)",
