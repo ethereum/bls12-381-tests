9a96499c104a257c7152c04a533f44fb670647ab
Antonio Sanso <antonio.sanso@gmail.com>
2025-03-03 09:44:11 +0100

    readd mulG1 , mulG2 cases

--- main.py
+++ main.py
@@ -1780,6 +1780,8 @@ def case18_fail_pairing_check():
 test_kinds: Dict[str, Generator[Tuple[str, Any], None, None]] = {
     'add_G1': case01_add_G1,
     'add_G2': case02_add_G2,
+    'mul_G1': case03_mul_G1,
+    'mul_G2': case04_mul_G2,
     'map_fp_to_G1': case05_map_fp_to_G1,
     'map_fp2_to_G2': case06_map_fp2_to_G2,
     'msm_G1': case07_msm_G1,
@@ -1787,6 +1789,8 @@ test_kinds: Dict[str, Generator[Tuple[str, Any], None, None]] = {
     'pairing_check': case09_pairing_check,
     'fail_add_G1': case10_fail_add_G1,
     'fail_add_G2': case11_fail_add_G2,
+    'fail_mul_G1' : case12_fail_mul_G1,
+    'fail_mul_G2' : case13_fail_mul_G2,
     'fail_map_fp_to_G1': case14_fail_map_fp_to_G1,
     'fail_map_fp2_to_G2': case15_fail_map_fp2_to_G2,
     'fail_msm_G1': case16_fail_msm_G1,
