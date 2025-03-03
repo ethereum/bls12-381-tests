e94466c4f7c82dc9aa26f4086ed7750c772dfff8
Antonio Sanso <antonio.sanso@gmail.com>
2025-03-03 13:33:22 +0100

    fixing lint

--- main.py
+++ main.py
@@ -885,8 +885,8 @@ def case07_msm_G1():
                 PRIVKEYS[2], 32) + int_to_hex(int(H2[0]), 64) + (int_to_hex(int(H2[1]), 64)) + int_to_hex(
                 PRIVKEYS[3], 32) + int_to_hex(int(H3[0]), 64) + (int_to_hex(int(H3[1]), 64)) + int_to_hex(
                 PRIVKEYS[4], 32) + int_to_hex(int(H4[0]), 64) + (int_to_hex(int(H4[1]), 64)) + int_to_hex(
-                PRIVKEYS[5], 32) + int_to_hex(0, 128) + int_to_hex(0, 32) +
-                int_to_hex(int(H5[0]), 64) + (int_to_hex(int(H5[1]), 64)) + int_to_hex(
+                PRIVKEYS[5], 32) + int_to_hex(0, 128) + int_to_hex(0, 32) + int_to_hex(
+                int(H5[0]), 64) + (int_to_hex(int(H5[1]), 64)) + int_to_hex(
                 PRIVKEYS[6], 32),
         "Name": "bls_g1msm_multiple_with_point_at_infinity",
         "Expected": int_to_hex(int(g1multiexp[0]), 64) + (int_to_hex(int(g1multiexp[1]), 64)),
