83bcf7c895c1f38adc04ca945618d5546b097c78
Antonio Sanso <antonio.sanso@gmail.com>
2025-03-05 11:54:57 +0100

    improve formatting

--- main.py
+++ main.py
@@ -1192,13 +1192,12 @@ def case09_pairing_check():
         "Input": ""
             # G1 point 1
             + int_to_hex(int(G1[0]), 64)
-            + (int_to_hex(int(G1[1]), 64))
+            + int_to_hex(int(G1[1]), 64)
             # G2 point 1 (point at infinity)
             + int_to_hex(0, 256)
             # G1 point 2 (point at infinity)
-            + int_to_hex(0, 256)
-            # G2 point 2
             + int_to_hex(0, 128)
+            # G2 point 2
             + int_to_hex(int(G2[0].coeffs[0]), 64)
             + int_to_hex(int(G2[0].coeffs[1]), 64)
             + int_to_hex(int(G2[1].coeffs[0]), 64)
