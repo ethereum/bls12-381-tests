1e699faf5b58c3b76ea636f8eb5187409a0435bb
Antonio Sanso <antonio.sanso@gmail.com>
2025-03-07 14:07:10 +0100

    improving formatting

--- main.py
+++ main.py
@@ -2349,32 +2349,92 @@ def case16_fail_msm_G1():
         "Name": "bls_g1msm_empty_input"
         },
         {
-        "Input": int_to_hex(int(G1[0]), 63) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G1 point (short x coordinate)
+            + int_to_hex(int(G1[0]), 63)
+            + int_to_hex(int(G1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # P1 point
+            + int_to_hex(int(P1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32),
         "ExpectedError": "invalid input length",
         "Name": "bls_g1msm_short_input"
         },
         {
-        "Input": int_to_hex(int(G1[0]), 65) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G1 point (long x coordinate)
+            + int_to_hex(int(G1[0]), 65)
+            + int_to_hex(int(G1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # P1 point
+            + int_to_hex(int(P1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32),
         "ExpectedError": "invalid input length",
         "Name": "bls_g1msm_long_input"
         },
         {
-        "Input": int_to_hex(int(G1[0]) + q, 64) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G1 point (wrong encoding)
+            + int_to_hex(int(G1[0]) + q, 64)
+            + int_to_hex(int(G1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # P1 point
+            + int_to_hex(int(P1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32),
         "ExpectedError": "invalid fp.Element encoding",
         "Name": "bls_g1msm_invalid_field_element"
         },
         {
-        "Input": "10" + int_to_hex(int(G1[0]), 63) + (int_to_hex(int(G1[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        # G1 point (wrong element top bytes)
+        "Input": "10"
+        + int_to_hex(int(G1[0]), 63)
+        + int_to_hex(int(G1[1]), 64)
+        # scalar
+        + int_to_hex(int(2), 32)
+        # P1 point
+        + int_to_hex(int(P1[0]), 64)
+        + int_to_hex(int(P1[1]), 64)
+        # scalar
+        + int_to_hex(int(2), 32),
         "ExpectedError": "invalid field element top bytes",
         "Name": "bls_g1msm_violate_top_bytes"
         },
         {
-        "Input": int_to_hex(int(G1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G1 point (not on curve)
+            + int_to_hex(int(G1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # P1 point
+            + int_to_hex(int(P1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32),
         "ExpectedError": "invalid point: not on curve",
         "Name": "bls_g1msm_point_not_on_curve"
         },
         {
-        "Input": int_to_hex(int(G1_wrong_order[0]), 64) + (int_to_hex(int(G1_wrong_order[1]), 64)) + int_to_hex(int(2), 32) + int_to_hex(int(P1[0]), 64) + (int_to_hex(int(P1[1]), 64)) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G1 point (not in the correct subgroup)
+            + int_to_hex(int(G1_wrong_order[0]), 64)
+            + int_to_hex(int(G1_wrong_order[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # P1 point
+            + int_to_hex(int(P1[0]), 64)
+            + int_to_hex(int(P1[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32),
         "ExpectedError": "g1 point is not in the correct subgroup",
         "Name": "bls_g1msm_g1_not_in_correct_subgroup"
         }
