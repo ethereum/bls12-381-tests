518734fb0b84e87f15f07011571d8647297ebcaf
Antonio Sanso <antonio.sanso@gmail.com>
2025-03-07 10:57:14 +0100

    improving formatting

--- main.py
+++ main.py
@@ -1425,15 +1425,30 @@ def case08_msm_G2():
         "NoBenchmark": False
         },
         {
-        "Input": int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(int(17), 32),
+        "Input": ""
+            # point at infinity
+            + int_to_hex(0, 64) + int_to_hex(0, 64)
+            + int_to_hex(0, 64) + int_to_hex(0, 64)
+            # scalar
+            + int_to_hex(int(17), 32),
         "Name": "bls_g2msm_(x*inf=inf)",
         "Expected": int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64) + int_to_hex(0, 64),
         "Gas": int((1 * BLS12_G2MUL_GAS * BLS12_G2_MULTIEXP_DISCOUNT_TABLE[0][1]) / 1000),
         "NoBenchmark": False
         },
         {
-        "Input": int_to_hex(int(G2[0].coeffs[0]), 64) + int_to_hex(int(G2[0].coeffs[1]), 64) + int_to_hex(int(G2[1].coeffs[0]), 64) + int_to_hex(
-            int(G2[1].coeffs[1]), 64) + int_to_hex(int(2), 32) + int_to_hex(0, 256) + int_to_hex(int(2), 32),
+        "Input": ""
+            # G2 point
+            + int_to_hex(int(G2[0].coeffs[0]), 64)
+            + int_to_hex(int(G2[0].coeffs[1]), 64)
+            + int_to_hex(int(G2[1].coeffs[0]), 64)
+            + int_to_hex(int(G2[1].coeffs[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # point at infinity
+            + int_to_hex(0, 256)
+            # scalar
+            + int_to_hex(int(2), 32),
         "Name": "bls_g2msm_(2g2+inf)",
         "Expected": int_to_hex(int(result_doubling_G2[0].coeffs[0]), 64) + int_to_hex(int(result_doubling_G2[0].coeffs[1]), 64) + int_to_hex(
             int(result_doubling_G2[1].coeffs[0]), 64) + int_to_hex(int(result_doubling_G2[1].coeffs[1]), 64),
@@ -1441,8 +1456,18 @@ def case08_msm_G2():
         "NoBenchmark": False
         },
         {
-        "Input": int_to_hex(int(P2[0].coeffs[0]), 64) + int_to_hex(int(P2[0].coeffs[1]), 64) + int_to_hex(int(P2[1].coeffs[0]), 64) + int_to_hex(
-            int(P2[1].coeffs[1]), 64) + int_to_hex(int(2), 32) + int_to_hex(0, 256) + int_to_hex(int(2), 32),
+        "Input": ""
+            # P2 point
+            + int_to_hex(int(P2[0].coeffs[0]), 64)
+            + int_to_hex(int(P2[0].coeffs[1]), 64)
+            + int_to_hex(int(P2[1].coeffs[0]), 64)
+            + int_to_hex(int(P2[1].coeffs[1]), 64)
+            # scalar
+            + int_to_hex(int(2), 32)
+            # point at infinity
+            + int_to_hex(0, 256)
+            # scalar
+            + int_to_hex(int(2), 32),
         "Name": "bls_g2msm_(2p2+inf)",
         "Expected": int_to_hex(int(result_doubling_P2[0].coeffs[0]), 64) + int_to_hex(int(result_doubling_P2[0].coeffs[1]), 64) + int_to_hex(
             int(result_doubling_P2[1].coeffs[0]), 64) + int_to_hex(int(result_doubling_P2[1].coeffs[1]), 64),
